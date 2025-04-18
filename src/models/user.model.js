cf01d3e1a8c6045697e2d998b61c87191798ca46
nirmal fageria <nirmalfageria15@gmail.com>
2025-04-19 00:22:18 +0530

    jwt token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -50,9 +50,34 @@ UserSchema.pre("save", async function (next) {
   next();
 });
 
+UserSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-UserSchema.methods.isPasswordCorrect = async function(password){
-  return await bcrypt.compare(password,this.password)
-}
+UserSchema.methods.generateAccesssToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.userName,
+      fullName: this.fullName,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECERT,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
+UserSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECERT,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
 export const User = mongoose.model("User", UserSchema);
