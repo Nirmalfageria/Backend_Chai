5e82ad226b04e8b609e857578e92e3fb940c9bdd
nirmal fageria <nirmalfageria15@gmail.com>
2025-04-19 16:09:58 +0530

    cloudinary part

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,22 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secert: process.env.CLOUDINARY_API_SECERT,
+});
+
+const cloudinaryUploader = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file is uploaded to the cloudinary", response);
+    return response;
+  } catch {
+    fs.unlinkSync(localFilePath)
+    // remove the file from the local cloud
+  }
+};
